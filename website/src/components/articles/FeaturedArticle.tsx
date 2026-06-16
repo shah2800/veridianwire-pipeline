f18b97faf78a21cef364e9d65a89dc38f67e5e69
shah2800 <shahzaman2800@gmail.com>
2026-06-17 00:57:32 +0500

    Make hero banner more compact on mobile to reduce screen-filling height

--- website/src/components/articles/FeaturedArticle.tsx
+++ website/src/components/articles/FeaturedArticle.tsx
@@ -25,7 +25,7 @@ export function FeaturedArticle({ article }: FeaturedArticleProps) {
 
   return (
     <article className="group relative overflow-hidden rounded-xl bg-slate-900">
-      <Link href={`/news/${article.slug}`} className="relative block aspect-[3/4] min-h-[380px] sm:aspect-[21/9] sm:min-h-[320px]">
+      <Link href={`/news/${article.slug}`} className="relative block min-h-[280px] sm:aspect-[21/9] sm:min-h-[320px]">
         <ArticleImage
           article={article}
           alt={title}
