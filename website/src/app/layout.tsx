251e3103548c2ecfce799a1a5659b1adcaa1e9f1
shah2800 <shahzaman2800@gmail.com>
2026-06-17 06:46:19 +0500

    Add Microsoft Clarity analytics tracking

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SITE, getSiteUrl } from '@/lib/site';
 import './globals.css';
 
 const GA_MEASUREMENT_ID = 'G-28HT27SQE0';
+const CLARITY_PROJECT_ID = 'x87szeenjg';
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
@@ -88,6 +89,15 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
           `}
         </Script>
       </body>
